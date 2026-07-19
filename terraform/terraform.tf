ed6e5fddb95f90eaa5db1b94a0592633f125e562
Srishti-cloud <Srishti-cloud@github.com>
2026-07-19 18:04:53 +0530

    Initial commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket-112"
+    key    = "s3-backend"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "6.55.0"
     }
   }
 }
